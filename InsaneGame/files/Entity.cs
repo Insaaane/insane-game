f051a7bdd19407729b649f79481f82f953e63d8d
Alexander <dayboval_official@mail.ru>
2023-06-01 21:17:35 +0500

    remake again

---
+++ InsaneGame/files/Entity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsaneGame.files
+{
+    public abstract class Entity
+    {
+
+    }
+}
